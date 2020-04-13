b7e9ad689eaf7e006f779af0783aef39c3f170ac
Danny White <hi@dannywhite.studio>
2020-04-13 13:15:01 +1000

    getting modules up and running

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Ephemera
 
 A digital archive of analogue bits and bobs.
+
+## Running this
+### Local server
+`npx serve`
+http://localhost:5000
+    
+### Sass
+`sass --watch style.scss style.css`
---
+++ js/app.js
@@ -0,0 +1,16 @@
+import {testVar} from './test.js';
+import data from './data.js'
+import slugify from './slugify.js'
+
+// console.log(data);
+
+// for (let i = 0; i < data.length; i++) {
+//     console.log(data[i].name);
+//     console.log(slugify(data[i].name));
+// }
+
+// const test = slugify("hi there");
+// console.log(test);
+
+// Prepare object for tags and their counts
+// const counts = {};
